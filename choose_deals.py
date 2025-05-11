7f4662de2e1b6cf323636ca6bd4fb92c6af83f1e
Alexey Dergunov <alexey.dergunov@gmail.com>
2025-05-11 15:40:37 +0300

    Script for choosing deals (3)

--- choose_deals.py
+++ choose_deals.py
@@ -8,7 +8,7 @@ from random import Random
 def main():
     logging.basicConfig(level=logging.INFO, format="")
 
-    deal_map: dict[str, int] = defaultdict(int)  # filename -> number of unique outcomes
+    deal_map: dict[str, list[str]] = defaultdict(list)  # filename -> list of unique outcomes
     with open("_infinite_log.txt", "r") as f:
         filename = None
         for line in f:
@@ -17,15 +17,15 @@ def main():
                 filename = line[line.rindex("/") + 1:]
             elif "->" in line:
                 assert filename is not None
-                deal_map[filename] += 1
+                deal_map[filename].append(line[line.index("("):])
 
     logging.info("Parsed %d deals", len(deal_map))
-    for i, (filename, count) in enumerate(sorted(deal_map.items(), key=lambda t: (-t[1], t[0]))):
-        logging.info("%d. Filename %s -> %d unique outcomes", i, filename, count)
+    for i, (filename, outcomes_list) in enumerate(sorted(deal_map.items(), key=lambda t: (-len(t[1]), t[0]))):
+        logging.info("%d. Filename %s -> %d unique outcomes", i, filename, len(outcomes_list))
 
     chosen_filenames = []
-    for filename, count in deal_map.items():
-        if count >= 5:
+    for filename, outcomes_list in deal_map.items():
+        if len(outcomes_list) >= 5:
             chosen_filenames.append(filename)
     assert len(chosen_filenames) >= 96
     r = Random("2025-05-11 15:30")  # fixed seed
@@ -40,13 +40,20 @@ def main():
     for i, filename in enumerate(chosen_filenames):
         game = i // 12 + 1
         deal = i % 12 + 1
-        logging.info("Game %d, deal %d -> %s, %d outcomes",
-                     game, deal, filename, deal_map[filename])
+        logging.info("Game %d, deal %d -> %s, %d outcomes:",
+                     game, deal, filename, len(deal_map[filename]))
+        for outcome in deal_map[filename]:
+            logging.info("  %s", outcome)
         src_file = os.path.join("wall_pictures", filename)
         dst_file = os.path.join(archive_directory, f"{game}_{deal:02d}_{filename}")
         shutil.copy(src_file, dst_file)
+
+    archive_name = "_chosen_deals_archive.zip"
     logging.info("Creating archive...")
-    shutil.make_archive("_chosen_deals_archive", "zip", archive_directory)
+    if os.path.exists(archive_name):
+        os.remove(archive_name)
+    shutil.make_archive(archive_name.split(".")[0], "zip", archive_directory)
+    logging.info("Archive created: %s bytes", os.path.getsize(archive_name))
 
 
 if __name__ == "__main__":
